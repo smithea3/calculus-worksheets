b3fa51b36520b88f105389f4c2e522927101adf7
Ethan A. Smith <smithea3@outlook.com>
2023-10-16 16:22:11 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,50 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "sec-section-name",
+  "level": "1",
+  "url": "sec-section-name.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Section Title",
+  "body": " Section Title  Text of section.    Solve the following equation.         "
+},
+{
+  "id": "exercise-1",
+  "level": "2",
+  "url": "sec-section-name.html#exercise-1",
+  "type": "Exercise",
+  "number": "1.1.1",
+  "title": "",
+  "body": " Solve the following equation.       "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
